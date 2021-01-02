b095cc6c96862f6102eed857921fdc276842c985
Nabil <nkoney14@outlook.com>
2021-01-02 13:44:56 -0500

    built navbar using reactstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
 import 'bootstrap/dist/css/bootstrap.min.css'; // ICLUDES BOOTSTRAP
 import './App.css';
 
@@ -12,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <AppNavbar />
       </div>
     )
   }
--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -7,7 +7,8 @@ import {
     Nav, // wrap links
     NavItem, // nav link
     NavLink, // has href value
-    Container // moves everything to middle
+    Container, // moves everything to middle
+    Col
 } from 'reactstrap';
 
 
@@ -24,13 +25,38 @@ class AppNavbar extends Component{
     state = {
             isOpen: false
         }
-        
+    
+    // TOGGLING NAVBAR
     toggle = () => {
         // SET THIS STATE...
         this.setState({
             isOpen: !this.state.isOpen // ... TO WHATEVER IT'S NOT
         });
-    }  
+    }
+    
+    // RENDER FUNCTION WITH REACTSTRAP
+    render(){
+        return (
+        <div>
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Container>
+                    <NavbarBrand href="/">Shopping List</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle}/>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="https://github.com/nabilkoneylaryea">
+                                    Github
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </div>
+        );
+    }
+    
 }
 
 export default AppNavbar;
